7e54eedf3393e1578c1ea587db123eda4a6c6029
jim-ubuntu <jimferguson_3@protonmail.com>
2025-07-06 18:31:35 +0100

    Made a landing screen, played with curses library

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
 #include <ncurses.h>
+
+
+
+int main()
+{
+  
+  initscr();
+  int xCenter {getmaxx(stdscr)/2};
+  int yCenter {getmaxy(stdscr)/2};
+  move(yCenter, xCenter);
+  printw("Welcome to pong\n");
+  refresh();
+  getch();
+  clear();
+  move(yCenter,xCenter);
+  printw("---##---\n");
+  move(yCenter + 1, xCenter);
+  printw("-######-\n");
+  move(yCenter + 2, xCenter);
+  printw("-######-\n");
+  move(yCenter + 3, xCenter);
+  printw("---##---");
+  refresh();
+  getch();
+  clear();
+  endwin();  
+  return 0;
+
+
+}
