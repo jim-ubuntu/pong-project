5cccad419caa8ec136a6fd2f8fe4e525bb700c8c
jim-ubuntu <jimferguson_3@protonmail.com>
2025-07-26 14:37:14 +0100

    Implemented player paddle. Now need to implement CPU paddle with PRNG as well as the ball, which uses prng initially, then serves to losing player?

--- main.cpp
+++ main.cpp
@@ -273,7 +273,7 @@ int main()
   init_pair(1, COLOR_RED, COLOR_WHITE);
   init_pair(2, COLOR_GREEN, COLOR_BLACK);
   init_pair(3, COLOR_BLACK, COLOR_GREEN);
-  init_pair(4, COLOR_BLUE, COLOR_BLACK);
+  init_pair(4, COLOR_BLACK, COLOR_BLACK);
   // cbreak();
   bool menu {true};
   int key {};
